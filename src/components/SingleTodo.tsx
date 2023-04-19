8484a2d6fbd44f5bd603a94f17eb900b2d679fe7
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-04-19 00:30:26 -0600

    added done feature

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Todo } from "../model";
+import { FaUserEdit } from "react-icons/fa";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { MdDoneOutline } from "react-icons/md";
+import "./styles.css";
+import TodoList from "./TodoList";
 
-const SingleTodo: React.FC = () => {
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+
+    // function for marking todo as done
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
-    <div>
+    <form className="todos__single">
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
 
-    </div>
-  )
-}
+      <div>
+        <span className="icon">
+          <FaUserEdit />
+        </span>
+        <span className="icon">
+          <RiDeleteBin5Fill />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDoneOutline />
+        </span>
+      </div>
+    </form>
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
