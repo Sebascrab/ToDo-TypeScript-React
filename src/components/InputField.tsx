39f02387e58074406d31d847f8192a8369fae75e
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-04-18 22:32:14 -0600

    logic to take state of todo entered on go

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className='input'>
-        <input type='input' placeholder='Enter A To-Do'className='input__box' />
+    <form className='input' onSubmit={handleAdd}>
+        <input 
+        value={todo}
+        onChange={
+            (e) => setTodo(e.target.value)
+        }
+        type='input' placeholder='Enter A To-Do'className='input__box' />
         <button className='input_submit' type='submit'>Go</button>
     </form>
   )
