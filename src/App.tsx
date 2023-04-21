7077fe592bae72a95f6b28dcef39dde6aea843ba
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-04-20 23:45:21 -0600

    added drop feature from beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import React, { useState } from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import TodoList from './components/TodoList';
-import { Todo } from './model';
-
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
+import { DragDropContext } from 'react-beautiful-dnd';
 
 
 const App: React.FC = () => {
-
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if(todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       setTodo("");
     }
   };
 
-  console.log(todos)
+  console.log(todos);
 
   return (
+    <DragDropContext>
     <div className="App">
-      <span className='heading'>To-Do</span>
+      <span className="heading">To-Do</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-      
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}
+      />
     </div>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
